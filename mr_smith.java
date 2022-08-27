fbc10f6f265e9d85240fdb834831ce4041892f63
rajgupta89 <gupta77raj123@gmail.com>
2022-08-27 13:12:23 +0530

    commit

---
+++ mr_smith.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class mr_smith{
+
+public static boolean isPalindrome(String s)
+{
+	int left = 0;
+	int right = s.length() - 1;
+
+	while (left < right)
+	{
+		if (s.charAt(left) !=
+			s.charAt(right))
+		{
+			return false;
+		}
+		left++;
+		right--;
+	}
+	return true;
+}
+
+public static void closestPalindrome(int num)
+{
+	
+	int RPNum = num - 1;
+	
+	while (isPalindrome(Integer.toString(RPNum)) == false)
+	{
+		RPNum--;
+	}
+	
+	int SPNum = num + 1;
+	
+	while (isPalindrome(Integer.toString(SPNum)) == false)
+	{
+		SPNum++;
+	}
+
+	if (Math.abs(num - SPNum) <
+		Math.abs(num - RPNum))
+	{
+		System.out.println(SPNum);
+	}
+	else
+		System.out.println(RPNum);
+}
+
+public static void main(String[] args)
+{
+    Scanner in=new Scanner(System.in);
+	int n = in.nextInt();
+	
+	closestPalindrome(n);
+}
+}
+
